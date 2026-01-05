9ed279574d54cfd79f26d12673e736e06c73d5bf
IPNCarterasConcentrado <Carteras.baxcalibour@gmail.com>
2026-01-05 13:54:33 -0600

    Update Codigo.js

--- Codigo.js
+++ Codigo.js
@@ -722,7 +722,7 @@ document.getElementById("miFormulario").addEventListener("keydown", function (e)
                                  body: new FormData(this)
                 })*/
                 
-                 fetch("https://script.google.com/macros/s/AKfycbwk10xInTUpzXbagna9BVLxNMs1hVIpC-4UUi51_HOx9HAxDp_zBH4Cx0XWOokqAw1JDg/exec", {
+                 fetch("https://script.google.com/macros/s/AKfycbw5xA7aUNZNtIy_oEIT0t1HwkgtUIVw8MWMn8ALD4QGwe0HFc0SKZnU6IuhQBk6p6rBPQ/exec", {
                   method: "POST",
                   body: JSON.stringify(data)
   })
@@ -741,6 +741,7 @@ document.getElementById("miFormulario").addEventListener("keydown", function (e)
   })
   .catch(() => alert("Error al enviar"));
 });
+
 
 
 
